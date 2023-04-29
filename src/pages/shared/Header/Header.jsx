d6ad386636749a400ddaccfc5ceed8dceaa4ecdc
Md-Azad <sokalazad@gmail.com>
2023-04-29 02:44:19 +0200

    added navbar

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
 import Marquee from "react-fast-marquee";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
@@ -22,6 +22,24 @@ const Header = () => {
           or just some text.
         </Marquee>
       </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="secondary">Secondary</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Container>
   );
 };
