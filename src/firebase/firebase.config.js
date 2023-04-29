3ab8514d4a69689bda67ee1d7ebe9897ed019f6a
Md-Azad <sokalazad@gmail.com>
2023-04-29 02:20:09 +0200

    the dragon news project initialized and one header part is done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfsV_7YNs3PPB_jAxFjW1c2rtmrFKdPrQ",
+  authDomain: "the-news-dragon-ca144.firebaseapp.com",
+  projectId: "the-news-dragon-ca144",
+  storageBucket: "the-news-dragon-ca144.appspot.com",
+  messagingSenderId: "1005642043380",
+  appId: "1:1005642043380:web:3f63e3c42ef124f5c673a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>Copyright fo my news.</h3>
+    </div>
+  );
+};
+
+export default Footer;
