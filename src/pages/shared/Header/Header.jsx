f8a83eb6e79fa67ddd5dca805a5c1294fa5ffc39
Md-Azad <sokalazad@gmail.com>
2023-04-29 02:34:01 +0200

    added react-fast marquee for the latest news part

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
+import Marquee from "react-fast-marquee";
+import { Button, Container } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p>
@@ -12,7 +14,15 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee pauseOnHover="true" speed={100}>
+          I can be a React component, multiple React components, or just some
+          text..........I can be a React component, multiple React components,
+          or just some text.
+        </Marquee>
+      </div>
+    </Container>
   );
 };
 
