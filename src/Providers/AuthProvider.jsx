90f13a4b4b86d68b57f7aed12c1af0d09748edca
Md-Azad <sokalazad@gmail.com>
2023-05-01 14:39:17 +0200

    added login and register page

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -5,7 +5,7 @@ import app from "../firebase/firebase.config";
 export const AuthCon = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-  const user = { displayName: "azad" };
+  const user = null;
   const authInfo = { user };
   return <AuthCon.Provider value={authInfo}>{children}</AuthCon.Provider>;
 };
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,15 +1,10 @@
 import logo from "../../../assets/logo.png";
 import moment from "moment";
 import Marquee from "react-fast-marquee";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { FaRegUserCircle } from "react-icons/fa";
-import { useContext } from "react";
-
+import { Button, Container } from "react-bootstrap";
 import { AuthCon } from "../../../Providers/AuthProvider";
 
 const Header = () => {
-  const { user } = useContext(AuthCon);
   return (
     <Container>
       <div className="text-center">
@@ -27,38 +22,7 @@ const Header = () => {
           or just some text.
         </Marquee>
       </div>
-      <div className="my-4">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mx-auto">
-                <Nav.Link href="#features">
-                  <Link to="/">Home</Link>
-                </Nav.Link>
-                <Nav.Link href="#pricing">About</Nav.Link>
-                <Nav.Link href="#pricing">Career</Nav.Link>
-              </Nav>
-              <Nav>
-                {user && (
-                  <Nav.Link href="#deets">
-                    <FaRegUserCircle
-                      style={{ fontSize: "2rem" }}
-                    ></FaRegUserCircle>
-                  </Nav.Link>
-                )}
-                <Nav.Link eventKey={2} href="#memes">
-                  {user ? (
-                    <Button variant="secondary">LogOut</Button>
-                  ) : (
-                    <Button variant="secondary">Login</Button>
-                  )}
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
+      <div className="my-4"></div>
     </Container>
   );
 };
